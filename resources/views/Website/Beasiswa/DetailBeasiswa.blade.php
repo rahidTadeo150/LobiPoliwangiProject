5c79ec2e793501ec35e250faa0c7cad497e65bd1
rahidTadeo150 <Akagitsune1010@gmail.com>
2024-12-02 16:04:15 +0700

    2 Desember 2024

---
+++ resources/views/Website/Beasiswa/DetailBeasiswa.blade.php
@@ -0,0 +1,34 @@
+@extends('Layout.WebsiteLayout')
+
+@section('Content')
+    <div class="fixed w-10">
+        <div class="relative w-[348px] h-[641px] overflow-hidden">
+            <div class="bg-black absolute z-[21] w-full h-full opacity-35"></div>
+            <img class="scale-150 blur-[2px]" src="\storage\DummyTesting\Turnamen-2021-Mercu-Buana-Mobile-Lengend-894x1264.jpg">
+        </div>
+    </div>
+    <div class="pl-[558px] pt-[55px]">
+        <div class="w-fit text-[13px] text-white font-medium py-[2px] px-[25px] bg-[#2F39FF] rounded-full mb-[10px]">
+            <p>Internasional</p>
+        </div>
+        <p class="text-[28px] font-bold mb-[14px]">Lomba Mobile Legend Bang Bang</p>
+        <div class="flex flex-row gap-x-[15px] items-center mb-[20px]">
+            <div class="w-[45px] h-[45px] rounded-full overflow-hidden">
+                <img src="\storage\DummyTesting\MandiriProfile.png">
+            </div>
+            <div class="text-[12px]">
+                <p class="font-medium">Bank Mandiri</p>
+                <p class="font-normal">mandiricare@mandiri.ac.id</p>
+            </div>
+        </div>
+        <div class="w-[750px] text-[12px] line-clamp-4">
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit tellus, auctor sed enim at, lacinia tempus nibh. Morbi felis nunc, rhoncus nec sollicitudin ac, lacinia eu libero. Morbi mauris nulla, iaculis sagittis quam id, consectetur maximus erat. Ut sit amet consequat urna, in eleifend lacus. Sed pellentesque egestas urna, at tristique sapien convallis non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam mollis eleifend tellus eu mollis. Vestibulum eleme  Nam mollis eleifend tellus eu mollis. Vestibulum eleme Nam mollis eleifend tellus eu mollis. Vestibulum eleme Nam mollis eleifend tellus eu mollis. Vestibulum eleme"</p>
+        </div>
+        <div>
+
+        </div>
+    </div>
+    <div class="fixed z-20 w-[362px] h-[437px] top-[128px] left-[126px] bg-black rounded-[10px]">
+        <img class="w-full h-full" src="\storage\DummyTesting\Turnamen-2021-Mercu-Buana-Mobile-Lengend-894x1264.jpg">
+    </div>
+@endsection
